3793ebacd4a2b1e77b3ab8d89233e475c3cd24ba
suryapavan1912 <pavansurya1912@gmail.com>
2023-04-16 18:57:40 +0530

    v1

---
+++ client/src/pages/homepage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/navbar';
+import Banner from '../components/banner';
+
+function Homepage() {
+  return (
+        <div>
+            <Navbar />
+            <Banner />
+        </div>
+    )
+}
+
+export default Homepage;
